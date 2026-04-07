a23ef0d7428da6be08da3c414baed6b5010596c7
Pradip-Bera <pradipbera450475@gmail.com>
2026-04-08 00:50:03 +0530

    fixed model loading with gdown

--- main.py
+++ main.py
@@ -3,21 +3,41 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 import io
+import gdown
+import os
 
 app = FastAPI()
 
-# Load model
-model = tf.keras.models.load_model("best_brain_tumor_model.h5")
+# -------------------------------
+# Model Download (Google Drive)
+# -------------------------------
+MODEL_PATH = "best_brain_tumor_model.h5"
 
+if not os.path.exists(MODEL_PATH):
+    url = "https://drive.google.com/uc?export=download&id=1W-PUtTe8CJN6395B0g0AU8LnCeLNJL6r"
+    gdown.download(url, MODEL_PATH, quiet=False)
+
+# Load model
+model = tf.keras.models.load_model(MODEL_PATH)
+
+# -------------------------------
+# Config
+# -------------------------------
 IMG_SIZE = 224
 CLASS_NAMES = ['glioma', 'meningioma', 'no_tumor', 'pituitary']
 
+# -------------------------------
+# Preprocessing
+# -------------------------------
 def preprocess_image(image):
     image = image.resize((IMG_SIZE, IMG_SIZE))
     image = np.array(image) / 255.0
     image = np.expand_dims(image, axis=0)
     return image
 
+# -------------------------------
+# Routes
+# -------------------------------
 @app.get("/")
 def home():
     return {"message": "Brain Tumor Detection API Running"}
